1d9528b51650cfcec457aab14deb35f43facfe1e
Weexy <146097877+Weexyy@users.noreply.github.com>
2024-06-01 21:58:32 +0800

    Add files via upload

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -737,11 +737,11 @@
           v-if="!isElectron"
           href="https://vercel.com/?utm_source=ohmusic&utm_campaign=oss"
         >
-         <img
-           height="36"
-           src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
-         />
-       </a>
+          <img
+            height="36"
+            src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
+          />
+        </a>
       </div>
     </div>
   </div>
